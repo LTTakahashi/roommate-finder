028af8b212d1f12039ed4bebfdb6fd4bf46f07a1
Keagasourus <keagen@tarmori.com>
2025-03-29 21:12:53 -0700

    Formatting
    added central formatting and crimson nav bar

--- roommatefinder/settings.py
+++ roommatefinder/settings.py
@@ -119,6 +119,11 @@ USE_TZ = True
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
