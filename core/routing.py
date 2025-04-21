8e2949ec26367c6e8d873a96bc58ada507031c95
LTTakahashi <l.takahashidosreis@wsu.edu>
2025-04-21 21:14:01 +0000

    chat

--- core/routing.py
+++ core/routing.py
@@ -1,6 +1,8 @@
+# routing.py (modified routing)
+
 from django.urls import re_path
-from .consumers import ChatConsumer
+from . import consumers
 
 websocket_urlpatterns = [
-    re_path(r'ws/chat/(?P<room_name>\w+)/$', ChatConsumer.as_asgi()),
+    re_path(r'ws/chat/(?P<room_name>[^/]+)/$', consumers.ChatConsumer.as_asgi()),
 ]
