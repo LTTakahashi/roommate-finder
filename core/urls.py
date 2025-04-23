c1f137b3760cd07566856430d47bc23305f096ea
LTTakahashi <l.takahashidosreis@wsu.edu>
2025-04-23 03:28:56 +0000

    UI

--- core/urls.py
+++ core/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
     path('profile/', views.profile, name='profile'),
-    path('search/', views.search_profiles, name='search_profiles'),
+    path('search/', views.search_profiles, name='search'),
     path('chat/<str:username>/', views.chat_room, name='chat_room'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
